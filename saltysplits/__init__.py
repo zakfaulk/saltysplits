230f048e4078f4e86aefc6606565fb32145d0506
Jasper <j.siebring92@gmail.com>
2025-03-27 21:15:38 +0100

    Included additional demo LSS file

--- saltysplits/__init__.py
+++ saltysplits/__init__.py
@@ -3,7 +3,6 @@ from .main import SaltySplits
 from .enums import TimeType
 from functools import partial as _partial
 
-#DEMO_SPLITS = pathlib.Path( __file__).parents[1] / "tests/run_files/Grand Theft Auto Vice City - Any% (No SSU).lss"
 DEMO_SPLITS = pathlib.Path( __file__).parents[1] / "tests/run_files/gcb.lss"
 
 # forwarding read_lss to main module (+ docs)
